21ad23ddebe7ed88a9bddbfdbe13f664fe4ddcb4
cedri <cedric.boutin56@gmail.com>
2026-08-22 13:37:29 +0200

    Implementing Tests

---
+++ Bases/Avae.ViewModels.Tests/TViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Avae.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Avae.ViewModels.Tests;
+
+public abstract class TViewModel<T> : IIoc, IDisposable where T : IViewModelBase
+{
+    public TViewModel()
+    {
+        Init();
+    }
+
+    public virtual void Init()
+    {
+        var services = new ServiceCollection();
+        Configure(services);
+        Configure(services.BuildServiceProvider());
+    }
+
+    public virtual void Configure(IServiceCollection services)
+    {
+
+    }
+
+    public virtual void Configure(IServiceProvider serviceProvider)
+    {
+        ServiceLocator.SetDefault(serviceProvider);
+    }
+
+    public virtual void Dispose()
+    {
+        //Messenger.Instance.Unregister(this);
+    }
+}
---
+++ Samples/Example.ViewModels.Tests/HomeViewModelTest.cs
@@ -0,0 +1,8 @@
+﻿using Avae.ViewModels.Tests;
+
+namespace Example.ViewModels.Tests;
+
+internal class HomeViewModelTest : TViewModel<HomeViewModel>
+{
+
+}
