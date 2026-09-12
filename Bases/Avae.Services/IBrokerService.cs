cbd21fd60b0218af8e2926d08c071adee3b92b30
cedri <cedric.boutin56@gmail.com>
2026-09-12 18:50:41 +0200

    Some refactoring

--- Bases/Avae.Services/IBrokerService.cs
+++ Bases/Avae.Services/IBrokerService.cs
@@ -7,3 +7,30 @@ public interface IBrokerService
     void Publish(Message message);
     void Subscribe(string topic, Action<Message> callback);
 }
+
+public class BrokerService : IBrokerService
+{
+    private readonly Dictionary<string, List<Action<Message>>> _subscriptions = [];
+
+    public void Publish(Message message)
+    {
+        if (_subscriptions.TryGetValue(message.Topic, out var callbacks))
+        {
+            foreach (var callback in callbacks)
+            {
+                callback(message);
+            }
+        }
+    }
+
+    public void Subscribe(string topic, Action<Message> callback)
+    {
+        if (!_subscriptions.TryGetValue(topic, out var value))
+        {
+            value = [];
+            _subscriptions.Add(topic, value);
+        }
+
+        value.Add(callback);
+    }
+}
